5fc60e7a6335d6869e098e68eeb2c27efc22f301
awr417h <abdullah@aw.com>
2018-07-28 01:33:00 +0500

    minor refactoring

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var socket = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 //Setting Express
 app.use(express.static('public'));
 
 //Setting up sockets
-socket.on('connection', function(socket) {
+io.on('connection', function(socket) {
     console.log('user connected ->', socket.id);
+
+    //Recieving Call backs
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
 });
 
 //Setting up server for listening
